505a49de42f89040182b37c0c6f7b0a3c47afa87
Saket Choudhary <saketkc@gmail.com>
2014-08-14 16:48:22 -0700

    redefine sources

--- FATHMM/fathmm.py
+++ FATHMM/fathmm.py
@@ -1,3 +1,13 @@
 import requests
 __url__="http://supfam3.cs.bris.ac.uk/FATHMM/cgi-bin/submit.cgi"
 
+__type__="CANCER" ##Hidden field to show which type of variants we are processing
+user_input = "P43026 L441P"
+user_threshold = 0.75
+data = {"batch": user_input,
+        "threshold": user_threshold,
+        "weighted": __type__}
+
+response = requests.post(__url__, data=data)
+print response.url
+print response.text
